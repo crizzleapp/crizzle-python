b11fdc97ad0499db3ed67440618acd4125f5b110
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2018-12-05 07:30:43 +0800

    Improved Logging System
    Added a centralized logging module (crizzle.utils.log)

--- crizzle/examples/binance.py
+++ crizzle/examples/binance.py
@@ -1,12 +1,7 @@
-import logging
 import crizzle
 
-logging.basicConfig(format='%(asctime)s -- %(levelname)s: %(message)s', handlers=[logging.StreamHandler()],
-                    level=logging.DEBUG)
-
+crizzle.logging.set_min_log_level(2)
 crizzle.set_crizzle_dir('G:/Documents/CrizzleData')
 crizzle.set_key('binance', "G:/Documents/CrizzleData/keys/binance_test.json")
 feed = crizzle.get_feed('binance')
-# print(feed._match_symbols(['ETH.*', 'EOS.*', 'TRX.*', 'BTC.*']))
-feed.download_candlesticks(min_interval_seconds=300000, symbols=['EOSETH'])
-# print(feed.get_update_timestamps(min_interval_seconds=30000))
+feed.download_candlesticks(min_interval_seconds=300, symbols=['EOSETH'])
