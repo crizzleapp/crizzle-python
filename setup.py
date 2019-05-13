25afc491329c55dc734f1cd600d894a3d6a1dd7b
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-05-13 16:00:18 +0800

    Added Requirements

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="crizzle",
-    version="0.0.1",
-    packages=["crizzle", ],
-    description="Python library for algorithmic trading on cryptocurrency exchanges",
-    author="Krishna Penukonda"
-)
